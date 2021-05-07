623f31b6c4d42881bfc5718575bed23afb51f9f5
neo hop <neotje111@gmail.com>
2021-05-07 12:46:30 +0200

    basic frontend

--- setup.py
+++ setup.py
@@ -9,13 +9,14 @@ PROJECT_GITHUB_USERNAME = "neotje"
 PACKAGES = find_packages()
 
 REQUIRED = [
-    "pymongo==3.11.3",
-    "mongoengine==0.23.0",
-    "aiohttp==3.7.4.post0",
-    "aiohttp-session==2.9.0",
-    "dnspython==2.1.0",
-    "PyYAML==5.4.1",
-    "cryptography==3.4.7"
+    "pymongo>=3.11.3",
+    "mongoengine>=0.23.0",
+    "aiohttp>=3.7.4.post0",
+    "aiohttp-session>=2.9.0",
+    "dnspython>=2.1.0",
+    "PyYAML>=5.4.1",
+    "cryptography>=3.4.7",
+    "Pillow>=8.0"
 ]
 
 setup(
